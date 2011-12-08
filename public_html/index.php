5b1ad8b1290ee22e95a60cc02d19a46920ec260b
hatone <b1006006@gmail.com>
2011-12-08 23:47:52 +0900

    fix index.php

--- public_html/index.php
+++ public_html/index.php
@@ -5,7 +5,7 @@ $test_env = $_SERVER['HTTP_X_FLX_FOO'];
 
 
 echo "hello fluxflex!!<br /><br />";
-echo "あなたのデータベースのデータベース名は".$database_name."です。<br />";
-echo "あなたのデータベースのパスワードは".$database_password."です。<br />";
-echo "あなたのSetENV:HTTP_X_FLX_FOOの値は".$test_env."です。<br />";
- ?>
+echo "データベースのデータベース名は".$database_name."です。<br />";
+echo "データベースのパスワードは".$database_password."です。<br />";
+echo "SetENV:HTTP_X_FLX_FOOの値は".$test_env."です。<br />";
+?>
