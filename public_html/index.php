291eaa7df20e51648be73e82d0bda24f100e6281
hatone <b1006006@gmail.com>
2011-12-08 22:45:27 +0900

    fix index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,11 @@
 <?php
+$database_name = 'DUMMY_DB_NAME';        
+$database_password = 'DUMMY_DB_PASSWORD';
 $test_env = $_SERVER['HTTP_X_FLX_HOGE'];
 
-echo $test_env;
+
+echo "hello fluxflex!!";
+echo "あなたのデータベースのデータベース名は".$database_name."です。";
+echo "あなたのデータベースのパスワードは".$database_password."です。";
+echo "あなたのSetENVの値は".$test_env."です。";
  ?>
